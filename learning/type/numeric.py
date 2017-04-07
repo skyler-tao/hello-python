18e1bc40812f7db27f5a5c0a1a6213134fe6435f
chaoqiang <chaoqiang@zhihu.com>
2017-04-07 08:16:47 +0800

    add type snippet

---
+++ learning/type/numeric.py
@@ -0,0 +1,4 @@
+my_positive_float = 32.3e3
+my_negative_float = 1.3e-2
+print my_positive_float
+print my_negative_float
---
+++ learning/type/string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print 'I\'m ok.'
+print 'I\'m learning\nPython.'
+print '\\\n\\'
+print '\\\t\\'
+
+print r'\\\t\\'     # '' 内部的字符串默认不转义，不加头部的 utf-8 会导致中文异常
